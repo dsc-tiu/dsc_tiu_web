c53d76fa305b0c0748a96f57a42c8722c0aa7474
Atri Das <babinsamrat123@gmail.com>
2019-12-19 22:41:48 +0530

    adjusts some designs

--- lib/screens/team_member_section.dart
+++ lib/screens/team_member_section.dart
@@ -11,26 +11,33 @@ class TeamMemberSection extends StatelessWidget {
         children: <Widget>[
           Container(
             height: MediaQuery.of(context).size.height / 2,
-            width: MediaQuery.of(context).size.width/2-150,
-            child: Text('Meet with our Team........',style: TextStyle(
-              fontSize: 80.0,
-              color: Colors.blueAccent,
-            ),),
+            width: MediaQuery.of(context).size.width / 2 - 100,
+            child: Text(
+              'Meet with our Team........',
+              style: TextStyle(
+                fontSize: 80.0,
+                color: Colors.blueAccent,
+              ),
+            ),
           ),
           Container(
             height: MediaQuery.of(context).size.height / 2,
-            width: MediaQuery.of(context).size.width/2,
-            child: ListView.builder(
-              physics: BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              itemCount: 6,
-              itemBuilder: (_, int index) {
-                return TeamMemberModel(
-                  memberPic: null,
-                  memberName: null,
-                  memberDesignation: null,
-                );
-              },
+            width: MediaQuery.of(context).size.width / 2 ,
+            child: Padding(
+              padding: const EdgeInsets.only(left:40.0),
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                padding: EdgeInsets.only(left: 40.0),
+                scrollDirection: Axis.horizontal,
+                itemCount: 6,
+                itemBuilder: (_, int index) {
+                  return TeamMemberModel(
+                    memberPic: null,
+                    memberName: null,
+                    memberDesignation: null,
+                  );
+                },
+              ),
             ),
           ),
         ],
