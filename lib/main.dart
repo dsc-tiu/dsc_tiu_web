d7d70bf9d4a113d23ffdecc433b04084948034f3
Atri Das <babinsamrat123@gmail.com>
2019-12-20 16:34:53 +0530

    adds appbar to contact us screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dsc_tiu_web/screens/contact_us__screen.dart';
+import 'package:dsc_tiu_web/screens/contact_us_screen.dart';
 import 'package:dsc_tiu_web/screens/event_screen.dart';
 import 'package:dsc_tiu_web/screens/gallery_screen.dart';
 import 'package:dsc_tiu_web/screens/splash_screen.dart';
@@ -9,18 +9,14 @@ import 'screens/home_screen.dart';
 main() => runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/',
+        initialRoute: '/contact-us-screen',
         routes: {
           '/': (_) => HomeScreen(),
           '/splash-screen': (_) => SplashScreen(),
           '/gallery-screen':(_)=>GalleryScreen(),
           '/event-screen': (_) => EventScreen(),
           '/team-member-screen': (_) => TeamMemberScreen(),
-          '/contact-us--screen': (_) => ContactUsScreen(),
+          '/contact-us-screen': (_) => ContactUsScreen(),
         },
       ),
 );
-
-//todo 1. internal Json for Team Members
-// todo 2. add some shadow or elevation to team member model
-// todo 3. add and about us text for DSC TIU
