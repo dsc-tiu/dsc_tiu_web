62fe51e8b0b80c21d78f2c4548b22e2a5f7c1142
Atri Das <babinsamrat123@gmail.com>
2019-12-20 18:25:39 +0530

    fixes somethings

--- lib/screens/contact_us_screen.dart
+++ lib/screens/contact_us_screen.dart
@@ -12,11 +12,13 @@ class ContactUsScreen extends StatelessWidget {
             child: Container(
               color: Colors.white,
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   Image.asset('asset/dsc_contact.jpg'),
                   Spacer(),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
                       RichText(
                         text: TextSpan(
@@ -57,13 +59,16 @@ class ContactUsScreen extends StatelessWidget {
                       ),
                       Align(
                         alignment: Alignment.bottomRight,
-                        child: Container(
-                          padding: EdgeInsets.only(
-                            right: 20.0,
+                        child: Card(
+                          elevation: 10.0,
+                          child: Container(
+                            padding: EdgeInsets.only(
+                              right: 60.0,
+                            ),
+                            height: MediaQuery.of(context).size.height/2,
+                            width: MediaQuery.of(context).size.width/2.7,
+                            color: Colors.greenAccent,
                           ),
-                          height: MediaQuery.of(context).size.height/2,
-                          width: MediaQuery.of(context).size.width/2.5,
-                          color: Colors.greenAccent,
                         ),
                       ),
                     ],
