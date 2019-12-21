d3522ca1baaf748e15c30410976071d859ad11ca
Atri Das <babinsamrat123@gmail.com>
2019-12-21 18:26:11 +0530

    initial track to splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'screens/home_screen.dart';
 main() => runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/',
+        initialRoute: '/splash-screen',
         routes: {
           '/': (_) => HomeScreen(),
           '/splash-screen': (_) => SplashScreen(),
